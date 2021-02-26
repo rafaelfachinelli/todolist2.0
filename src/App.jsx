938b8def6148a8f183b5a104566258412a8a0d3f
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-02-26 16:32:14 -0300

    chore: project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Routes from "./routes";
+
+import Menu from "./components/Menu";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Menu />
+      <Routes />
+    </div>
+  );
+}
