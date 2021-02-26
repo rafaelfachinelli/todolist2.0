de9806ac882c4290e5271622a0c3793faecaf5b1
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-02-26 16:34:39 -0300

    feat(about): initial structure

---
+++ src/views/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "../components/Header";
+
+export default function About() {
+  return (
+    <div>
+      <Header name="Sobre" small="Nós" />
+      <h2>Nossa história</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      <h2>Missão, Visão e Valores</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+      <h2>Imprensa</h2>
+      <p>Lorem ipsum dolor sit amet...</p>
+    </div>
+  );
+}
